168a1929cdaf60eebe357ebd665f972be337afd7
Yoteichi <plonk@piano.email.ne.jp>
2014-05-06 22:05:46 +0900

    TypeAssociationクラスを作った

---
+++ type_association.rb
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+require 'singleton'
+require_relative 'launcher'
+
+class TypeAssociation
+  include Singleton
+
+  def initialize
+    @identity_map = {}
+  end
+
+  def launcher(type)
+    Settings[:TYPE_ASSOC].each do |type_pattern, cmdline|
+      if type =~ /^#{type_pattern}$/
+        unless @identity_map.has_key? cmdline
+          @identity_map[cmdline] = Launcher.new(cmdline)
+        end
+        return @identity_map[cmdline]
+      end
+    end
+    nil
+  end
+end
