453ee3867eb54059e812fb2ebcd22ebec582158d
Yoteichi <plonk@piano.email.ne.jp>
2014-05-08 01:55:17 +0900

    リファクタリング

--- type_assoc_dialog.rb
+++ type_assoc_dialog.rb
@@ -7,42 +7,54 @@ class TypeAssocDialog < Gtk::Dialog
 
   def initialize parent
     super("タイプ関連付け", parent, Dialog::MODAL)
+
     @type_assoc = ::Settings[:TYPE_ASSOC]
 
-    self.set_size_request(480,320)
+    layout
+    load
+  end
 
-    @object_list = ObjectList.new(['タイプ', 'コマンドライン'], 
-                                  [
-                                   proc { |obj| obj[0] },
-                                   proc { |obj| obj[1] }
-                                  ],
-                                  [
-                                   proc { |obj, val| obj[0] = val }, 
-                                   proc { |obj, val| obj[1] = val },
-                                  ])
-    @object_list.set @type_assoc
+  def load
+    @assoc_list.set @type_assoc
+  end
+
+  def layout
+    self.set_size_request(480,320)
+    self.vbox.spacing = 5
+
+    @assoc_list = create_assoc_list
 
     create(HBox, false, 5) do |hbox|
-      hbox.pack_start(@object_list, true)
+      hbox.pack_start(@assoc_list, true)
 
-      create(VButtonBox, spacing: 5, layout_style: ButtonBox::START) do |bbox|
-        @add_button = create(Button, Stock::ADD, sensitive: false)
-        bbox.pack_start(@add_button, false)
-        @delete_button = create(Button, Stock::DELETE, sensitive: false)
-        bbox.pack_start(@delete_button, false)
+      create(VButtonBox,
+             layout_style: ButtonBox::START,
+             spacing: 5) do |bbox|
+        buttons = [Stock::ADD, Stock::DELETE, Stock::GO_UP, Stock::GO_DOWN]
+          .map { |type| create(Button, type, sensitive: false) }
+
+        [:pack_start, :pack_start, :pack_end, :pack_end].zip(buttons)
+          .each do |pack, button|
+          bbox.send(pack, button, false)
+        end
+        @add_button, @delete_button, @up_button, @down_button = buttons
+
         hbox.pack_start(bbox, false)
-
-        @up_button = create(Button, Stock::GO_UP, sensitive: false)
-        bbox.pack_end(@up_button, false)
-        @down_button = create(Button, Stock::GO_DOWN, sensitive: false)
-        bbox.pack_end(@down_button, false)
       end
-      vbox.pack_start(hbox)
+      self.vbox.pack_start(hbox)
     end
-    vbox.spacing = 5
-    vbox.pack_end(HSeparator.new, false)
+    self.vbox.pack_end(HSeparator.new, false)
 
-    add_button(Stock::OK, RESPONSE_OK)
+    add_button(Stock::OK,     RESPONSE_OK)
     add_button(Stock::CANCEL, RESPONSE_CANCEL)
+  end
+
+  def create_assoc_list
+    headers = ['タイプ', 'コマンドライン']
+    readers = [ proc { |obj| obj[0] },
+                proc { |obj| obj[1] } ]
+    writers = [ proc { |obj, val| obj[0] = val },
+                proc { |obj, val| obj[1] = val } ]
+    return ObjectList.new(headers, readers, writers)
   end
 end
