59d6ac94661bafcb2ead51431ec5e10a052910e0
Yoteichi <plonk@piano.email.ne.jp>
2014-05-16 08:56:08 +0900

    データを分けた

--- spec/bandwidth_checker.rb
+++ spec/bandwidth_checker.rb
@@ -4,47 +4,10 @@ require 'webmock'
 require 'open-uri'
 require_relative '../channel'
 require_relative '../extensions'
+require_relative 'test_data'
 
-ordinary_channel =
-  ['予定地',
-   'AE91BBC0A36C2F8C73DA9556860D7890',
-   '218.45.166.124:7144',
-   'http://jbbs.shitaraba.net/bbs/read.cgi/game/48538/1395690547/',
-   'プログラミング',
-   'Ruby - &lt;Free&gt;',
-   '-1',
-   '-1',
-   '497',
-   'WMV',
-   '',
-   '',
-   '',
-   '',
-   '%E4%BA%88%E5%AE%9A%E5%9C%B0',
-   '0:34',
-   'click',
-   '',
-   '0'].join('<>')
-uptest_channel =
-  ['アップロード帯域',
-   '00000000000000000000000000000000',
-   '',
-   'http://naiyo.yoteichi.com/uptest/',
-   '',
-   'No data 帯域測定はコンタクトURL から',
-   '-1',
-   '-1',
-   '',
-   'RAW',
-   '',
-   '',
-   '',
-   '',
-   '',
-   '0:00',
-   'click',
-   '',
-   '0'].join('<>')
+ordinary_channel = ORDINARY_CHANNEL_LINE
+uptest_channel = UPTEST_CHANNEL_LINE
 index_txt = [ordinary_channel, uptest_channel].join("\n") + "\n"
 WebMock.stub_request(:get, 'http://naiyo.yoteichi.com/index.txt')
   .to_return(:body => index_txt)
