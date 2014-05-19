055e9f38c5ee639f3bee1b8de616cc5b0a127f99
Yoteichi <plonk@piano.email.ne.jp>
2014-05-19 17:01:38 +0900

    Page クラスを別ファイルにした

---
+++ notebook_page.rb
@@ -0,0 +1,16 @@
+class Page < Gtk::VBox
+  include Observable
+  attr_reader :title, :label
+
+  def initialize
+    super()
+
+    @label = TabLabel.new(self)
+  end
+
+  def title= str
+    @title = str
+    changed
+    notify_observers
+  end
+end
